7e7e4fa12bae8a00b4289437e5ee43eb4e63566b
Jeremy Bond <bond_jeremy_w@outlook.com>
2021-06-01 13:39:08 -0400

    Small Changes

--- jeremybondnet/src/app/app.component.spec.ts
+++ jeremybondnet/src/app/app.component.spec.ts
@@ -23,13 +23,13 @@ describe('AppComponent', () => {
   it(`should have as title 'jeremybondnet'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('jeremybondnet');
+    expect(app.title).toEqual('jeremybond.net');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('jeremybondnet app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('jeremybond.net app is running!');
   });
 });
